bb5b7aaed9d6b0bcd9040de1343be48bfe5ca1a0
aaron-santos <aaron-kevin.santos@stud.fh-campuswien.ac.at>
2023-12-27 16:22:20 +0100

    add express for backend

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -5,7 +5,7 @@ export default function LoginForm() {
                 <div class="col-12 col-sm-8 col-md-6 m-auto">
 <div class="card border-0 shadow">
     <div class="card-body text-center">
-    <svg class="mx-auto my-3" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+    <svg class="mx-auto my-3 bi bi-person-circle" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"  viewBox="0 0 16 16">
   <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
   <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
 </svg>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Use the '/api' prefix for all backend requests
+      '/api': {
+        target: 'http://localhost:3000', // Backend server URL
+        changeOrigin: true,
+        secure: false,
+        // Optionally, rewrite the '/api' prefix in the request path
+        // to the root '/' if your backend routes don't have the '/api' prefix
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+});
