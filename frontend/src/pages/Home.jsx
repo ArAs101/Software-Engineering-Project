b3069737321e89bca0ecd1873678c74c44ab20af
aaron-santos <aaron-kevin.santos@stud.fh-campuswien.ac.at>
2023-12-27 17:19:42 +0100

    add giftawayentry

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import Navbar from '../components/Navbar'
+import GiftAwayEntry from '../components/GiftawayEntry'
 
 export default function Home() {
     return (
         <div>
-            <Navbar />
+            <div>
+                <Navbar />
+            </div>
+            <div>
+                <GiftAwayEntry />
+            </div>
         </div>
     )
 }
