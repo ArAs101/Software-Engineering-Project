557ff8993230b6a769cf0b03df84ec7f0a86d18f
ArAs101 <armin.assadzadeh@stud.fh-campuswien.ac.at>
2023-12-28 19:35:42 +0100

    Managed to connect Frontend and Backend

--- src/App.js
+++ src/App.js
@@ -1,42 +1,61 @@
-import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
+import Test from "./components/test";
 import ReactDOM from "react-dom/client";
-import GiftAwaysResults from "./components/GiftAwaysResults";
-import Categories from "./components/categories";
-import GALogo from "./GiftAwayLogo.png"
-import ClaimedGiftAwaysList from "./ClaimedGiftAways";
-
-ReactDOM.createRoot(document.getElementById('root'));
+import Register from "./components/Register";
+import {Routes, Route, useNavigate} from "react-router-dom";
 
 function App() {
+
+/*  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:4000');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);*/
+const navigate = useNavigate()
+
+    const navigateToRegisterPage = () => {
+      navigate(Register)
+    }
+
   return (
-      <div id="index">
+    <div className="App">
+      <Test/>
+        <h2>................................................</h2>
+        <button onClick={navigateToRegisterPage}>Register here</button>
+        <Routes>
+            <Route path={Register}/>
+        </Routes>
+        {/*<Register/>*/}
 
-        <div id="GALogo">
-          <img src={GALogo} id="GA_Logo"/>
-        </div>
-        <div className="TopRight">
-          <p id="Settings&LogOut">You are logged in as (Username).</p>
-        </div>
 
-        <div id="Categories">
-          <Categories/>
-        </div>
-
-        <div id="ResultsList">
-          <GiftAwaysResults/>
-        </div>
-        <div id="ClaimedList">
-          <ClaimedGiftAwaysList/>
-        </div>
-
-        <div className="footer">
-          <p>Copyright 2023 GiftAway.</p>
-        </div>
-      </div>
-  )
+      {/*<header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>*/}
+    </div>
+  );
 }
-
 
 export default App;
---
+++ src/components/Register.js
@@ -0,0 +1,37 @@
+import React, {Component, useEffect, useState} from "react";
+
+function Register() {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:4000/register');
+                const jsonData = await response.json();
+                setData(jsonData);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchData();
+    }, []);
+
+
+    const [data, setData] = useState("");
+    return (
+        <div className="App">
+
+            <h2>{data ? <p>{data.message}</p> : <p>Loading...</p>}</h2>
+            {/*<header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.</p>
+
+
+                <a className="App-link" href="https://reactjs.org"
+                   target="_blank" rel="noopener noreferrer">
+                    Learn React</a>
+            </header>*/}
+        </div>
+    );
+}
+
+export default Register
